d905c092a94b5383de357d864686a65d4d1be344
jmh <takoowang@gmail.com>
2014-05-02 15:09:58 +0900

    nnn

---
+++ youngjin_sys/youngjin_sys/src/main/java/org/youngjin/net/OnhandSum.java
@@ -0,0 +1,26 @@
+package org.youngjin.net;
+
+public class OnhandSum {
+	private int sumGross;
+	private int sumNet;
+	private int sumCuft;
+	public int getSumGross() {
+		return sumGross;
+	}
+	public void setSumGross(int sumGross) {
+		this.sumGross = sumGross;
+	}
+	public int getSumNet() {
+		return sumNet;
+	}
+	public void setSumNet(int sumNet) {
+		this.sumNet = sumNet;
+	}
+	public int getSumCuft() {
+		return sumCuft;
+	}
+	public void setSumCuft(int sumCuft) {
+		this.sumCuft = sumCuft;
+	}
+	
+}
